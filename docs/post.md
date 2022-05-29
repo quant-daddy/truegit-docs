74a45a4e882d829e2dcc32a4baf4a8706c9f315e
Suraj Keshri <surajkeshri@gmail.com>
2022-05-29 10:02:31 -0400

    initial commit

---
+++ docs/post.md
@@ -0,0 +1,27 @@
+---
+sidebar_position: 2
+---
+
+```md title="temp.md"
+---
+# The date the post is published on.
+# Default: The file commit date.
+# Supports [Date.parse](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse) method
+# Examples: "YYYY-MM-DD", "Month Date, Year"
+date: 1989/07/11
+# The title of the post
+# Default: First H1 tag of your post
+title: My post's title
+subTitle: My post's sub title
+bannerImage: ./banner.png
+slug: this-is-the-slug
+description?: string;
+tags: [kubernetes, graphql]
+authors:
+  - author-1
+  - author-2
+disableSuggestEdit: false
+draft: false
+featured: true
+---
+```
---
+++ docusaurus.config.js
@@ -0,0 +1,77 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "My Site",
+  tagline: "Dinosaurs are cool",
+  url: "https://truegit.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "Truegit", // Usually your GitHub org/user name.
+  projectName: "Truegit", // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "My Site",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "create-a-blog",
+            position: "left",
+            label: "Docs",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} Truegit.io, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
